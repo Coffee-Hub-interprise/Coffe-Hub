849485cb38b71256fe8b071c8339d150bb341e92
ShadowDarkus <shadowdarkus05@gmail.com>
2025-04-21 22:36:53 -0300

    Last Version

--- src/Paginas/Menu/script.js
+++ src/Paginas/Menu/script.js
@@ -5,3 +5,30 @@ const navLinks = document.querySelector('.nav-links');
 toggleBtn.addEventListener('click', function() {
     navLinks.classList.toggle('show-links');
 });
+
+
+//=============================== Efeito de fade-in nos elementos ao rolar a página ===============================
+const fadeElements = document.querySelectorAll('.fade-in');
+
+function checkFade() {
+    fadeElements.forEach(element => {
+        const elementPosition = element.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight * 0.8;
+        
+        if (elementPosition < screenPosition) {
+            element.classList.add('show');
+        }
+    });
+}
+
+window.addEventListener('scroll', checkFade);
+window.addEventListener('load', checkFade);
+
+
+// Animação para ícones especiais
+const specialIcons = document.querySelectorAll('.special-icon');
+if (specialIcons.length > 0) {
+    specialIcons.forEach((icon, index) => {
+        icon.style.animationDelay = `${index * 0.5}s`;
+    });
+}
